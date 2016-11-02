ff3cebb05e67cb76d7f5432caf3791f74f8deb31
Peter Boström <git@pbos.me>
2016-11-01 22:21:15 -0400

    Add Bitrate Utilization to graphs.
    Clearly displays when an encoder is overshooting/undershooting.

--- generate_graphs.py
+++ generate_graphs.py
@@ -19,7 +19,7 @@ def generate_graphs(output_dict, graph_data, target_metric, bitrate_config):
       for layer in split_data(codec, 'temporal-layer'):
         metric_data = []
         for data in layer:
-          metric_data.append((int(data['target-bitrate-bps'])/1000, float(data[target_metric])))
+          metric_data.append((int(data['target-bitrate-bps'])/1000, float(data[target_metric]), float(data['bitrate-utilization'])))
         line_name = '%s-%s-tl%d' % (layer[0]['encoder'], layer[0]['codec'], layer[0]['temporal-layer'])
         lines[line_name] = metric_data
 
@@ -47,18 +47,25 @@ def main():
     metric = graph_name.split(':')[-1]
     fig, ax = plt.subplots(1)
     ax.set_title(graph_name)
+    ax.set_xlabel('Bitrate (kbps)')
+    ax.set_ylabel(metric.upper())
+    ax2 = ax.twinx()
+    ax2.set_ylabel('Bitrate Utilization')
     for title in sorted(lines.keys()):
       points = lines[title]
       x = []
       y = []
-      for xx, yy in points:
-          x.append(xx)
-          y.append(yy)
-      plt.plot(x,y,'o-', linewidth=1, label=title)
+      y2 = []
+      for bitrate_kbps, value, utilization in points:
+          x.append(bitrate_kbps)
+          y.append(value)
+          y2.append(utilization)
+      ax.plot(x,y,'o--', linewidth=1, label=title)
       ax.legend(loc='best', fancybox=True, framealpha=0.5)
-      ax.set_xlabel('Bitrate (kbps)')
-      ax.set_ylabel(metric.upper())
       ax.grid()
+      ax2.plot(x,y2, 'x-', alpha=0.2)
+    # Set bitrate limit axes to +/- 20%.
+    ax2.set_ylim(bottom=0.80, top=1.20)
     plt.savefig("out/%s.png" % graph_name.replace(":", "-"))
 
 if __name__ == '__main__':
